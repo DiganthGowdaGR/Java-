3a1f2d47aa74b555b4bf03f672830593fbeb6dc0
Sharath GR <160706150+DiganthGowdaGR@users.noreply.github.com>
2025-03-07 15:49:41 +0530

    Create dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,61 @@
+class test {
+
+    public static void main(String[] args) {
+
+        // The MAX and MIN range B/W byte, that we can use
+        System.out.println("<-------Byte Range------->");
+        byte a = 122;
+        System.out.println("Byte value: " + a);
+        System.out.println("Byte Min value: " + Byte.MIN_VALUE);
+        System.out.println("Byte Max value: " + Byte.MAX_VALUE);
+        System.out.println();
+
+        // The MAX and MIN range B/W short, that we can use
+        System.out.println("<-------Short Range------->");
+        short b = 2;
+        System.out.println("Short value: " + b);
+        System.out.println("Short Min value: " + Short.MIN_VALUE);
+        System.out.println("Short Max value: " + Short.MAX_VALUE);
+        System.out.println();
+
+        // The MAX and MIN range B/W Integer, that we can use
+        System.out.println("<-------Integer Range------->");
+        int c = 2;
+        System.out.println("Integer value: " + c);
+        System.out.println("Integer Min value: " + Integer.MIN_VALUE);
+        System.out.println("Integer Max value: " + Integer.MAX_VALUE);
+        System.out.println();
+
+        // The MAX and MIN range B/W Long, that we can use
+        System.out.println("<-------Long Range------->");
+        long d = 2L;
+        System.out.println("Long value: " + d);
+        System.out.println("Long Min value: " + Long.MIN_VALUE);
+        System.out.println("Long Max value: " + Long.MAX_VALUE);
+        System.out.println();
+
+        // The MAX and MIN range B/W Float, that we can use
+        System.out.println("<-------Float Range------->");
+        float e = 2.5f;
+        System.out.println("Float value: " + e);
+        System.out.println("Float Min value: " + Float.MIN_VALUE);
+        System.out.println("Float Max value: " + Float.MAX_VALUE);
+        System.out.println();
+
+        // The MAX and MIN range B/W Double, that we can use
+        System.out.println("<-------Double Range------->");
+        double f = 5.25;
+        System.out.println("Double value: " + f);
+        System.out.println("Double Min value: " + Double.MIN_VALUE);
+        System.out.println("Double Max value: " + Double.MAX_VALUE);
+        System.out.println();
+
+        // Character
+        System.out.println("<-------Character Range------->");
+        char g = 'A';
+        System.out.println("Character value: " + g);
+        System.out.println("Character Min value: " + Character.MIN_VALUE);
+        System.out.println("Character Max value: " + Character.MAX_VALUE);
+        System.out.println();
+    }
+}
