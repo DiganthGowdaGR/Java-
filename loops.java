bb255e6e9d4603d8b7e92e3407e4afd6e1f854b8
Sharath GR <160706150+DiganthGowdaGR@users.noreply.github.com>
2025-03-07 22:46:37 +0530

    Create loops.javajuhygvfc xz nlhgc

---
+++ loops.java
@@ -0,0 +1,27 @@
+class loops{
+
+    public static void main(String[] args) {
+        int i=1;
+
+        //While loop
+        while(i<=50){
+            System.out.println("hey, how are you");
+            i++; 
+        }
+        System.out.println(" ");
+
+        //for loop
+        for(int a=1; a<=10; a++){
+            System.out.println("count:"+a);
+        }
+        System.out.println(" ");
+
+        //do-while loop
+        int p=1;
+        do { 
+            System.out.println("hello");
+            p++;
+        }while(p<=10);
+        System.out.println(" ");
+    }
+}
